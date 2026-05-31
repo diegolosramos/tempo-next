1accba0ad800f04234851f1f6a2990e761553b75
Diego Ramos <54applets_hogs@icloud.com>
2026-05-31 13:30:53 -0400

    add wireframe

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import { Wireframe, WireframeNav } from "@/components/wireframe";
+
+export default function BaseLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Wireframe>
+      <WireframeNav
+        className="border-slate-200 border-b bg-background/80 backdrop-blur"
+        position="top"
+      >
+        <div className="mx-auto flex h-full w-full max-w-5xl items-center px-4 sm:px-6">
+          <div className="font-bold tracking-wide">Tempo Examples</div>
+        </div>
+      </WireframeNav>
+
+      <main className="mx-auto w-full max-w-6xl px-4 py-6 sm:px-6">
+        {children}
+      </main>
+    </Wireframe>
+  );
+}
